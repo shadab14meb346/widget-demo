74aa1df391ac5003056047974984d5408d2dc099
Shadab <shadab14meb346@gmail.com>
2021-10-04 14:16:04 +0530

    update the src link to cdn

--- widget-demo-entry.js
+++ widget-demo-entry.js
@@ -1,5 +1,5 @@
 (function (d, t) {
   t = d.createElement("script");
-  t.setAttribute("src", "/static/widget-demo-main.js");
+  t.setAttribute("src", "https://cdn.jsdelivr.net/gh/shadab14meb346/widget-demo/widget-demo-main.js");
   d.getElementsByTagName("head")[0].appendChild(t);
 })(document);
